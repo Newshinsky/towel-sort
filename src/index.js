6fc11e90a1fe00d883ac1b3ebbe6adef3c2f14ad
Newshinsky <72308491+Newshinsky@users.noreply.github.com>
2021-02-15 15:15:50 +0300

    feat: realize 'towelSort' function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+ matrix = matrix.flat(Infinity);
+  matrix.sort(function (a, b) {
+    return a - b
+  })
+
+  return matrix
 }
