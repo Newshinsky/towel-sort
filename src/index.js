0dd10552d563642ae7d5c031def98741391339cd
Newshinsky <72308491+Newshinsky@users.noreply.github.com>
2021-02-15 21:32:39 +0300

    fix: create a condition for missing arguments

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+let emptyArr = []
+if (arguments.length === 0) {
+return emptyArr;
+}
  for (i = 1; i < matrix.length; i+=2) {
     matrix[i] = matrix[i].reverse();
-  }
+  };
+ 
   matrix = matrix.flat(Infinity);
   return matrix;
 }
